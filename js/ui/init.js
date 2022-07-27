d0fd90f0d75ff1d0c5e7390fe54be5e3293031fa
Patrick Winters <19wintersp@gmail.com>
2022-07-27 23:04:26 +0100

    init: ReplOS says hello!

--- js/ui/init.js
+++ js/ui/init.js
@@ -2,5 +2,7 @@ import { setConsoleLogDomain } from 'console';
 
 setConsoleLogDomain('GNOME Shell');
 
+console.log("Good day my sunshine! ReplOS says hello!");
+
 imports.ui.environment.init();
 imports.ui.main.start();
